b2cd974166a5625f7d20797f301f5d22f86297e6
Alec Dhuse <alec@dhuse.com>
2015-05-21 10:23:49 -0400

    Mime type checking
    Mime type checking

--- server-side/add_photo.php
+++ server-side/add_photo.php
@@ -10,20 +10,42 @@
     if ($auth == true) {
         $data = $_POST['photo_data'];
         
-        //Write code to determin file name here
-        $file_name = "test.jpg";
+        //Check dataurl mime type
+        $mime_type = substr($data, 5, strpos($data, ";")-5);
         
-        $uri = substr($data,strpos($data,",")+1);
-        $encoded_data = str_replace(' ','+', $uri);
-        $decoded_data = base64_decode($encoded_data);
+        if (strcmp($mime_type, "image/png") == 0) {
+            $mime_type_ok = true;
+            $file_ext = ".png";
+        } else if (strcmp($mime_type, "image/jpeg") == 0) {
+            $mime_type_ok = true;
+            $file_ext = ".jpg";
+        } else {
+            $mime_type_ok = false;
+            $file_ext = "";
+        }
         
-        file_put_contents($file_name, $decoded_data);
-        
-        $results  = array(
-            'type'          => 'api_response',
-            'result'        => 'Photo upload successful.',
-            'result_code'   => 1,
-        );
+        if ($mime_type_ok == true) {
+            //Write code to determin file name here
+            $file_name = "test" . $file_ext;
+            
+            $uri = substr($data,strpos($data,",")+1);
+            $encoded_data = str_replace(' ','+', $uri);
+            $decoded_data = base64_decode($encoded_data);
+            
+            file_put_contents($file_name, $decoded_data);
+            
+            $results  = array(
+                'type'          => 'api_response',
+                'result'        => 'Photo upload successful.',
+                'result_code'   => 1,
+            );
+        } else {
+            $results  = array(
+                'type'          => 'api_response',
+                'result'        => 'Unsupported file type.',
+                'result_code'   => -1,
+            );
+        }
     } else {
         $results  = array(
             'type'          => 'api_response',
